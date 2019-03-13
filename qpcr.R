d8ef41bf3cd20a27ac5c6235178a5f81ed5b3fe5
Marc Galland <m.galland@uva.nl>
2019-03-13 10:53:55 +0100

    Create qpcr.R

---
+++ qpcr.R
@@ -0,0 +1,8 @@
+# In the tutorial they added the samples-names by hand, I made it now that it will just take the first row from the excel sheet and use this as for samples
+qPCR = read.csv("qPCR.csv", header = TRUE)
+samples <- qPCR[,1]
+data <- qPCR[-c(1)]
+res <- pcr_analyze(data,
+                  group_var = samples,
+                  reference_gene = 'SAND',
+                  reference_group = 'WTC')
