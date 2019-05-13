f7057a380b9c88c0e2181d0085808690523bd1ad
mgalland <m.galland@uva.nl>
2019-05-13 16:18:34 +0200

    Now works with the balanced mock data set

--- qpcr.R
+++ qpcr.R
@@ -3,7 +3,7 @@ suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(RColorBrewer))
 
 # In the tutorial they added the samples-names by hand, I made it now that it will just take the first row from the excel sheet and use this as for samples
-qpcr = read.delim("qpcr-mock-data.tsv", header = TRUE,stringsAsFactors = F)
+qpcr = read.delim("qpcr-mock-data-balanced.tsv", header = TRUE,stringsAsFactors = F)
 
 # first, let's make a unique identifier by concatenating the xp_factor with the biol_rep columns
 qpcr$unique_id = paste(qpcr$xp_factor,qpcr$biol_rep,sep = "_")
@@ -28,7 +28,8 @@ qpcr %>%
   ggplot(aes(unique_id,exp_ct_vals)) + 
   geom_boxplot() +
   geom_point() +
-  facet_wrap(~ gene)
+  facet_wrap(~ gene,scales = "free") +
+  labs(x="Biological replicate",y="Expression (2^-Ct)",title = "Variation among technical replicates")
 
 ##########################################################
 # average the technical replicates of the reference genes
@@ -42,7 +43,7 @@ reference_values = qpcr %>%
 # assigning a unique identifier for left_join
 # we add the sample unique identifier to be able to combine the reference gene Ct values with the target genes Ct values
 # making sure that reference genes Ct values are next to 
-reference_values$unique_id = paste(reference_values$xp_factor,reference_values$biol_rep,sep="_") 
+  reference_values$unique_id = paste(reference_values$xp_factor,reference_values$biol_rep,sep="_") 
 
 # simplifying the reference_values dataframe
 reference_values = reference_values[,c("avg_reference","unique_id")]
@@ -57,28 +58,20 @@ qpcr.with.ref.values = dplyr::left_join(x = qpcr,y = reference_values,by="unique
 qpcr.with.ref.values = mutate(qpcr.with.ref.values,delta.ct=exp_ct_vals / avg_reference)
 head(qpcr.with.ref.values)
 
-# plot the technical replicates distances
-# qpcr.with.ref.values %>%  
-#   ggplot(aes(x=biol_rep.x,y=delta.ct,color=as.factor(techinical.replicat))) + 
-#   geom_point() + 
-#   geom_line(group=biol_rep.x) +
-#   scale_y_log10() +
-#   facet_wrap( ~ gene.x + xp_factor.x)
-
 ################################################
 # Average the technical replicates for all genes
 ################################################
 qpcr.with.ref.values.avg = qpcr.with.ref.values %>% 
-  group_by(xp_factor.x,biol_rep.x,gene.x) %>%
+  group_by(xp_factor,biol_rep,gene) %>%
   summarise(avg_tech = mean(delta.ct))
 
 # plot per xp_factor and facet per gene
 qpcr.with.ref.values.avg %>% 
-    ggplot(aes(x=xp_factor.x,y=avg_tech,fill=gene.x)) +
+    ggplot(aes(x=xp_factor,y=avg_tech,fill=gene)) +
   geom_boxplot() +
   geom_point() +
   scale_y_log10() +
-  facet_wrap(~ gene.x) +
+  facet_wrap(~ gene) +
   scale_color_brewer(palette = "Set2") +
   labs(x = "Experimental condition",y="Normalised expression (AU)") +
   theme(axis.text= element_text(color="black"))
